eee7ff7962d59dda173fffed3c91b298fbd8f372
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2013-06-21 11:18:10 +0200

    browse images in grid-view

--- items/browse.php
+++ items/browse.php
@@ -5,6 +5,7 @@ echo head(array('title'=>$pageTitle,'bodyclass' => 'items browse'));
 
 <h1><?php echo $pageTitle;?> <?php echo __('(%s total)', $total_results); ?></h1>
 
+<div class="items" style="padding-left:1em">
 <?php echo pagination_links(); ?>
 
 <?php if ($total_results > 0): ?>
@@ -14,41 +15,39 @@ $sortLinks[__('Title')] = 'Dublin Core,Title';
 $sortLinks[__('Creator')] = 'Dublin Core,Creator';
 $sortLinks[__('Date Added')] = 'added';
 ?>
+
 <div id="sort-links">
     <span class="sort-label"><?php echo __('Sort by: '); ?></span><?php echo browse_sort_links($sortLinks); ?>
 </div>
+</div>
 
 <?php endif; ?>
+<div class="items">
+    <?php foreach (loop('items') as $item): ?>
+    <div class="picture">
+        <p><strong><?php echo link_to_item(metadata('item', array('Dublin Core', 'Title')), array('class'=>'permalink')); ?></strong></p>
+        <div class="picture-img">
+            <?php echo link_to_item(item_image('square_thumbnail')); ?>
+        </div>
+        <div class="item-meta">
+        <?php if (metadata('item', 'has thumbnail')): ?>
+        <?php endif; ?>
 
-<?php foreach (loop('items') as $item): ?>
-<div class="item hentry">
-    <h2><?php echo link_to_item(metadata('item', array('Dublin Core', 'Title')), array('class'=>'permalink')); ?></h2>
-    <div class="item-meta">
-    <?php if (metadata('item', 'has thumbnail')): ?>
-    <div class="item-img">
-        <?php echo link_to_item(item_image('square_thumbnail')); ?>
-    </div>
-    <?php endif; ?>
+        <?php if ($description = metadata('item', array('Dublin Core', 'Description'), array('snippet'=>100))): ?>
+        <div class="item-description">
+            <?php echo $description; ?>
+        </div>
+        <?php endif; ?>
 
-    <?php if ($description = metadata('item', array('Dublin Core', 'Description'), array('snippet'=>250))): ?>
-    <div class="item-description">
-        <?php echo $description; ?>
-    </div>
-    <?php endif; ?>
+        <?php fire_plugin_hook('public_items_browse_each', array('view' => $this, 'item' =>$item)); ?>
 
-    <?php if (metadata('item', 'has tags')): ?>
-    <div class="tags"><p><strong><?php echo __('Tags'); ?>:</strong>
-        <?php echo tag_string('items'); ?></p>
-    </div>
-    <?php endif; ?>
-
-    <?php fire_plugin_hook('public_items_browse_each', array('view' => $this, 'item' =>$item)); ?>
-
-    </div><!-- end class="item-meta" -->
-</div><!-- end class="item hentry" -->
-<?php endforeach; ?>
-
-<?php echo pagination_links(); ?>
+        </div><!-- end class="item-meta" -->
+    </div><!-- end class="item hentry" -->
+    <?php endforeach; ?>
+</div>
+<div class="items" style="padding-left:1em">
+   <?php echo pagination_links(); ?>
+</div>
 
 <?php fire_plugin_hook('public_items_browse', array('items'=>$items, 'view' => $this)); ?>
 
