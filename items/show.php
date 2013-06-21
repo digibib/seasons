cc133c0e19ab910f8388ee57b923353b07e68611
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2013-06-21 10:18:16 +0200

    remove prev and next item links on single item page

--- items/show.php
+++ items/show.php
@@ -42,9 +42,4 @@
 
 </aside>
 
-<ul class="item-pagination navigation">
-    <li id="previous-item" class="previous"><?php echo link_to_previous_item_show(); ?></li>
-    <li id="next-item" class="next"><?php echo link_to_next_item_show(); ?></li>
-</ul>
-
 <?php echo foot(); ?>
