f1e2ef18a9ddc55fb59d3e9a320d1c16736649ba
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2013-06-21 13:30:25 +0200

    gridresults in search

--- search/index.php
+++ search/index.php
@@ -5,24 +5,28 @@ $searchRecordTypes = get_search_record_types();
 ?>
 <?php echo search_filters(); ?>
 <?php if ($total_results): ?>
-<?php echo pagination_links(); ?>
-<table id="search-results">
-    <thead>
-        <tr>
-            <th><?php echo __('Record Type');?></th>
-            <th><?php echo __('Title');?></th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php foreach (loop('search_texts') as $searchText): ?>
+    <div class="items" style="padding-left:1em">
+        <?php echo pagination_links(); ?>
+    </div>
+    
+<div class="items">
+    <?php foreach (loop('search_texts') as $searchText): ?>
         <?php $record = get_record_by_id($searchText['record_type'], $searchText['record_id']); ?>
-        <tr>
-            <td><?php echo $searchRecordTypes[$searchText['record_type']]; ?></td>
-            <td><a href="<?php echo record_url($record, 'show'); ?>"><?php echo $searchText['title'] ? $searchText['title'] : '[Unknown]'; ?></a></td>
-        </tr>
-        <?php endforeach; ?>
-    </tbody>
-</table>
+        <?php if(get_class($record) =='Item'):
+              set_current_record('Item',$record);
+              $item = get_current_record('Item');          
+             ?>
+
+             <div class="picture">
+                 <p><strong><?php echo link_to_item(metadata('item', array('Dublin Core', 'Title')), array('class'=>'permalink')); ?></strong></p>
+                 <div class="picture-img">
+                     <?php $description = metadata('item', array('Dublin Core', 'Description'), array('snippet'=>100)) ?>
+                     <?php echo link_to_item(item_image('square_thumbnail', array('title' => $description))); ?>
+                 </div>
+             </div>
+        <?php endif;?>
+    <?php endforeach; ?>
+</div>
 <?php echo pagination_links(); ?>
 <?php else: ?>
 <div id="no-results">
