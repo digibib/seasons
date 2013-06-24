e9e8b04c85d99455c280ffa3784cdf7d8d939330
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2013-06-24 11:22:25 +0200

    dc.creator -> fotograf

--- common/record-metadata.php
+++ common/record-metadata.php
@@ -7,7 +7,7 @@
     <div id="<?php echo text_to_id(html_escape("$setName $elementName")); ?>" class="element">
         <h3><?php echo html_escape(__($elementName)); ?></h3>
         <?php foreach ($elementInfo['texts'] as $text): ?>
-            <?php if (in_array(strtolower($elementName), array("subject", "date", "avbildet person", "fotograf", "gårdsnavn", "sted", "bydel"))): ?>
+            <?php if (in_array(strtolower($elementName), array("subject", "date", "avbildet person", "fotograf", "creator",  "gårdsnavn", "sted", "bydel"))): ?>
                 <div class="element-text"><a href='/omeka/items/browse?search=&advanced%5B0%5D%5Belement_id%5D=<?php echo $elementInfo['element']->id ;?>&advanced%5B0%5D%5Btype%5D=contains&advanced%5B0%5D%5Bterms%5D=<?php echo $text; ?>&range=&collection=&type=&user=&tags=&public=&featured=&submit_search=Søk'><?php echo $text; ?></a></div>
             <?php else: ?>
                 <div class="element-text"><?php echo $text; ?></div>
