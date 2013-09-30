3f5b3c13f64cfb1151825e8532c4fe9b1cd60b97
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2013-09-30 12:16:12 +0200

    enable search in collection

--- collections/show.php
+++ collections/show.php
@@ -16,13 +16,13 @@ $total_results=metadata('Collection', 'total_items');
     <p><?php echo link_to_items_browse('Bla i alle ('.$total_results.') bildene til ' . metadata('Collection',array('Dublin Core','Title')), array('collection' => $coll)); ?></p>
 
 </div>
-<!-- <div style="padding-left:1em">
-    <form name="search-form" action="/omeka/items/browse" method="get">
+<div style="padding-left:1em">
+    <form name="search-form" action="/items/browse" method="get">
         <label><strong>Søk i samlingen: </strong><input type="text" name="search" style="max-width:200px"></label>
         <input type="hidden" name="collection" value="<?php echo $coll;?>">
         <input type="submit" name="submit_search" value="søk">
     </form>
-</div> -->
+</div>
 
 <div style="padding-left:1em">
     <h2>Bilder fra samlingen</h2>
